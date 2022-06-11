581661a706c00e9189b7362e2fdbb20d6fb6e927
iamdarshangowda <iamdarshangowda@gmail.com>
2022-06-11 16:26:14 +0530

    direction length updated

--- src/Components/Dialog.js
+++ src/Components/Dialog.js
@@ -45,9 +45,9 @@ function Dialog() {
       y: dragDivDetails.top + e.clientY - dragStart.y,
     };
     if (
-      divPosition.y > 295 ||
+      divPosition.y > 285 ||
       divPosition.y < 55 ||
-      divPosition.x + 200 > document.documentElement.clientWidth ||
+      divPosition.x + 300 > document.documentElement.clientWidth ||
       divPosition.x < 0
     ) {
       setDragging(false);
